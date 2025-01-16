63ad539151fd05b8d4cdba7251064022f2aab9cd
VitorOSZz <harlerquin224@gmail.com>
2025-01-16 20:57:47 -0300

    A lot of changes on css, made Simple Interest Calculator
    Simple Interest Calculator made, maybe will change in the future but now is functionally and use table, and changes on css

--- Assets/Scripts/scriptSimpleInterest.js
+++ Assets/Scripts/scriptSimpleInterest.js
@@ -1,9 +1,11 @@
 function SimpleInterest(InitialValue,InterestRate,Time,InterestTime,TypeOfTime) {
     InitialValue = parseInt(InitialValue)
     InterestRate = InterestRate/100
-
+    
+    div = document.querySelector("#Result")
     if (InitialValue == '' || InterestRate == '' || Time == '') {
-        return console.log('Say a number')
+        console.log('Say a number')
+        return div.innerHTML = '<p>Por favor, preencha todos os campos</p>'
     }
     console.log(`Initial Value :${InitialValue}`) 
     console.log(`Interest Rate :${InterestRate}`) 
@@ -22,16 +24,24 @@ function SimpleInterest(InitialValue,InterestRate,Time,InterestTime,TypeOfTime) 
         console.log('Time Correct')
     }
 
-    /* "J = C ∙ i ∙ t" */
+
+    
     Interest = 0
+    Month = 0
+    /* Start of table, with the Month 0*/
+    txt = `<table><tr><th>Mês</th><th>Juros</th><th>Total Investido</th><th>Total Juros</th><th>Total Acumulado</th></tr><tr><td>${Month}</td><td>${(InitialValue*InterestRate).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td><td>${InitialValue.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td><td>${Interest.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td><td>${(InitialValue+Interest).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td></tr>`
     while (Time > 0) {
+        Month +=1
 
         Interest = Interest + ( InitialValue*InterestRate)
 
-        console.log(Interest, 'LOL', Time)
+        console.log(Interest, ' | ', Time)
+        txt += `<tr><td>${Month}</td><td>${(InitialValue*InterestRate).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td><td>${(InitialValue).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td><td>${(Interest).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td><td>${(InitialValue+Interest).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td></tr>`
         Time -=1
     }
 
+    div.innerHTML = `${txt}</table>`
     console.log(InitialValue+Interest)
+
 
 }
